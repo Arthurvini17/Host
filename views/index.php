f5f78439d4293ce5e5bf552b7515cbb569ec7b79
Arthur <arthurdedsec@gmail.com>
2023-06-20 16:52:57 -0300

    commits

---
+++ views/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title>Host</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li><a href="#home">Página Inicial</a></li>
+                <li><a href="#">Planos</a></li>
+                <li><a href="#footer">Contato</a></li>
+                <li><a href="registrar.php"> Entrar </a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <div class="pai">
+            <div class="tit-inic">
+                <h1>Plataforma de host <br>completa para você ter<br> qualquer site </h1>
+                <h4>Hospedagem com até <span> 71%</span>OFF</h4>
+            </div>
+            <div class="ofertas">
+                <p>Dominio Grátis</p>
+                <p>Migração de sites Grátis</p>
+                <p>Suporte ao Cliente 24h</p>
+            </div>
+
+            <div class="preco">
+                <h1>R$ 12,99 <span>/mês</span> </h1>
+                <h4>+3 Meses Grátis</h4>
+                <button>Aproveitar Ofertas</button>
+            </div>
+        </div>
+        <div class="imagem">
+            <img src="../public/imagens/home.svg" alt="">
+        </div>
+    </section>
+
+    <section id="planos">
+        <div class="pai">
+            <div class="premium">
+                <div class="header">
+                    <h1>Hospedagem Premium</h1>
+                    <h5>Perfeita para sites pessoais</h5>
+                </div>
+                <div class="preco">
+                    <h1>R$ 12,99 <span>/mês</span></h1>
+                    <p>Preço assinatura de 48 meses</p>
+                    <button>Adicionar ao Carrinho</button>
+                </div>
+                <div class="itens">
+                    <h1>Comparação dos principais recursos</h1>
+                    <p>100 Sites</p>
+                    <p>Largura de Banda Ilimitada</p>
+                    <p>100 GB de Armazenamento SSD</p>
+                    <p>Domínio Grátis (preço R$49,99)</p>
+                    <p>SSL Ilimitado Grátis</p>
+                    <p>Backups Semanais</p>
+
+                    <h1>Desempenho</h1>
+                    <p>1 GB RAM</p>
+                    <p>1 Núcleo de CPU</p>
+                    <p>Recursos Dedicados</p>
+                    <p>IP Dedicado</p>
+
+                    <h1>Segurança</h1>
+                    <p>Nameserver Protegidos com a Cloudflare</p>
+                    <p>Detector de malware</p>
+
+                    <h1>Bônus Grátis</h1>
+                    <p>E-mail Grátis</p>
+                    <p>Migração de Sites Grátis</p>
+
+                    <h1> Opções WordPress </h1>
+                    <p>WordPress Gerenciado</p>
+                    <p>Aceleração WordPress</p>
+                    <p>WP-CLI</p>
+                    <p>WordPress Multisite</p>
+                    <p>Cache de Objetos para WordPress</p>
+                    <p>Ambiente de Teste do WordPress</p>
+
+                    <h1>Serviço e Suporte</h1>
+                    <p>30 dias para pedir reembolso</p>
+                    <p> Suporte online 24h</p>
+                    <p>99,9% de Uptime Garantido</p>
+                    <p> Servidores em vários países</p>
+                </div>
+            </div>
+
+            <div class="business">
+                <div class="header">
+                    <h1>Hospedagem Premium</h1>
+                    <h5>Perfeita para sites pessoais</h5>
+                </div>
+                <div class="preco">
+                    <h1>R$ 12,99 <span>/mês</span></h1>
+                    <p>Preço assinatura de 48 meses</p>
+                    <button>Adicionar ao Carrinho</button>
+                </div>
+                <div class="itens">
+                    <h1>Comparação dos principais recursos</h1>
+                    <p>100 Sites</p>
+                    <p>Largura de Banda Ilimitada</p>
+                    <p>100 GB de Armazenamento SSD</p>
+                    <p>Domínio Grátis (preço R$49,99)</p>
+                    <p>SSL Ilimitado Grátis</p>
+                    <p>Backups Semanais</p>
+
+                    <h1>Desempenho</h1>
+                    <p>1 GB RAM</p>
+                    <p>1 Núcleo de CPU</p>
+                    <p>Recursos Dedicados</p>
+                    <p>IP Dedicado</p>
+
+                    <h1>Segurança</h1>
+                    <p>Nameserver Protegidos com a Cloudflare</p>
+                    <p>Detector de malware</p>
+
+                    <h1>Bônus Grátis</h1>
+                    <p>E-mail Grátis</p>
+                    <p>Migração de Sites Grátis</p>
+
+                    <h1> Opções WordPress </h1>
+                    <p>WordPress Gerenciado</p>
+                    <p>Aceleração WordPress</p>
+                    <p>WP-CLI</p>
+                    <p>WordPress Multisite</p>
+                    <p>Cache de Objetos para WordPress</p>
+                    <p>Ambiente de Teste do WordPress</p>
+
+                    <h1>Serviço e Suporte</h1>
+                    <p>30 dias para pedir reembolso</p>
+                    <p> Suporte online 24h</p>
+                    <p>99,9% de Uptime Garantido</p>
+                    <p> Servidores em vários países</p>
+                </div>
+            </div>
+
+
+            <div class="startup">
+                <div class="header">
+                    <h1>Hospedagem Premium</h1>
+                    <h5>Perfeita para sites pessoais</h5>
+                </div>
+                <div class="preco">
+                    <h1>R$ 12,99 <span>/mês</span></h1>
+                    <p>Preço assinatura de 48 meses</p>
+                    <button>Adicionar ao Carrinho</button>
+                </div>
+                <div class="itens">
+                    <h1>Comparação dos principais recursos</h1>
+                    <p>100 Sites</p>
+                    <p>Largura de Banda Ilimitada</p>
+                    <p>100 GB de Armazenamento SSD</p>
+                    <p>Domínio Grátis (preço R$49,99)</p>
+                    <p>SSL Ilimitado Grátis</p>
+                    <p>Backups Semanais</p>
+
+                    <h1>Desempenho</h1>
+                    <p>1 GB RAM</p>
+                    <p>1 Núcleo de CPU</p>
+                    <p>Recursos Dedicados</p>
+                    <p>IP Dedicado</p>
+
+                    <h1>Segurança</h1>
+                    <p>Nameserver Protegidos com a Cloudflare</p>
+                    <p>Detector de malware</p>
+
+                    <h1>Bônus Grátis</h1>
+                    <p>E-mail Grátis</p>
+                    <p>Migração de Sites Grátis</p>
+
+                    <h1> Opções WordPress </h1>
+                    <p>WordPress Gerenciado</p>
+                    <p>Aceleração WordPress</p>
+                    <p>WP-CLI</p>
+                    <p>WordPress Multisite</p>
+                    <p>Cache de Objetos para WordPress</p>
+                    <p>Ambiente de Teste do WordPress</p>
+
+                    <h1>Serviço e Suporte</h1>
+                    <p>30 dias para pedir reembolso</p>
+                    <p> Suporte online 24h</p>
+                    <p>99,9% de Uptime Garantido</p>
+                    <p> Servidores em vários países</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="seguranca">
+
+    </section>
+
+    <section id="feedback">
+
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
